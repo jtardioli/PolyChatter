265e279e928072ed2c94b41ca407ddaac62d3b2f
jtardioli <joshua.tardioli@gmail.com>
2022-01-14 18:28:46 -0500

    set up basic server

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,13 @@ const usersRouter = require("./routes/users");
 const app = express();
 
 app.use(logger("dev"));
-app.use(express.json());
+app.use(express.json()); // req.body
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const pool = require("../db");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/", function (req, res) {
+  console.log(req.body);
 });
 
 module.exports = router;
