726d0d8d360b8e6a396d886fb96b3b0f93a68f79
jtardioli <joshua.tardioli@gmail.com>
2022-01-16 17:41:40 -0500

    create register page form with hooks

--- frontend/src/components/RegisterPage.js
+++ frontend/src/components/RegisterPage.js
@@ -1,28 +1,56 @@
+import { useState } from "react";
+
 const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser = () => {
+    console.log(name, userName, email, password);
+  };
+
   return (
     <div>
       <h1>Register</h1>
       <form>
         <label>
           Name
-          <input type="text" name="name" />
+          <input
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            name="name"
+          />
         </label>
 
         <label>
           Username:
-          <input type="text" name="username" />
+          <input
+            onChange={(e) => setUserName(e.target.value)}
+            type="text"
+            name="username"
+          />
         </label>
 
         <label>
           Email:
-          <input type="email" name="email" />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            name="email"
+          />
         </label>
 
         <label>
           Password:
-          <input type="password" name="password" />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            name="password"
+          />
         </label>
       </form>
+      <button onClick={registerUser}>Register</button>
     </div>
   );
 };
