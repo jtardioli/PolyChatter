d1409ecb60379e9f7790242bacfff81382e4ca7a
Justin Tu <coppolavina@gmail.com>
2022-01-16 05:05:18 +0000

    edit and add schema

--- backend/db/migrations/02_messages.sql
+++ backend/db/migrations/02_messages.sql
@@ -2,7 +2,9 @@ DROP TABLE IF EXISTS messages CASCADE;
 
 CREATE TABLE messages (
   id SERIAL PRIMARY KEY NOT NULL,
+  text TEXT,
+  translation TEXT,
+  userConversation_id INTEGER REFERENCES userConversations(id) ON DELETE CASCADE,
   sender_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  conversation_id INTEGER REFERENCES conversations(id) ON DELETE CASCADE,
   created_at timestamp
 );
