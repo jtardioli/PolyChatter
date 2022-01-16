9f1d1f775d1727dd27908d5848eb6db24ab3b3e3
jtardioli <joshua.tardioli@gmail.com>
2022-01-16 14:32:19 -0500

    create user registration route and query -without lang

--- backend/db/migrations/01_users.sql
+++ backend/db/migrations/01_users.sql
@@ -7,6 +7,6 @@ CREATE TABLE users (
   image TEXT,
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
-  country_id INTEGER NOT NULL,
+  country_id INTEGER,
   bio TEXT
 );
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,13 +4,26 @@ const { registerValidation } = require("../validators/auth");
 const {
   validationMiddleware,
 } = require("../middlewares/validations-middleware");
+const { hash } = require("bcryptjs");
 const pool = require("../db");
 
 const register = async (req, res) => {
+  const { username, name, email, password } = req.body;
   try {
-    console.log("validation passed");
+    const hashedPassword = await hash(password, 10);
+    await pool.query(
+      `INSERT INTO users (username, name, email, password)
+    VALUES ($1, $2, $3, $4);`,
+      [username, name, email, hashedPassword]
+    );
+    res.status(201).json({
+      success: true,
+      message: "Registration Successful",
+    });
   } catch (err) {
-    console.log(err.message);
+    return res.status(500).json({
+      error: err.message,
+    });
   }
 };
 
