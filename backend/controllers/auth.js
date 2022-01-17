40b547e9cc6d0ac2f3e81c4af9ea602d82393c77
jtardioli <joshua.tardioli@gmail.com>
2022-01-16 20:12:53 -0500

    add login for login page

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -4,6 +4,7 @@ const { sign } = require("jsonwebtoken");
 require("dotenv").config();
 
 exports.register = async (req, res) => {
+  console.log(req.body);
   const { username, name, email, password } = req.body;
   try {
     const hashedPassword = await hash(password, 10);
