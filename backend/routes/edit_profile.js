f29b8fa03b472834536b3c677248cb8e5222e23d
Gulbike Biyekenova <gbknva@gmail.com>
2022-01-18 04:11:46 +0000

    UPDATE query for image, username, name, info

--- backend/routes/edit_profile.js
+++ backend/routes/edit_profile.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
-const { userAuth } = require("../middlewares/auth-middleware");
+const { validateJWTTokenMiddleware } = require("../middlewares/auth-middleware");
 const cloudinary = require('cloudinary')
 const { hash } = require("bcryptjs");
 //const { cloudinaryConfig, uploader } = require('../config/cloudinaryConfig')
@@ -9,14 +9,16 @@ const { hash } = require("bcryptjs");
 
 // Get Users Information for Profile Page
 //userAuth
-router.post("/profile/edit", userAuth, async (req, res) => {
+router.post("/profile/edit", validateJWTTokenMiddleware, async (req, res) => {
   // console.log(req.body)
-   console.log(req.files)
+  console.log("Edit profile")
+   console.log(req.body)
   // console.log(req.user)
 
  try {
   let imageURL = "";
-  if ("files" in req) {
+  let userInformation = {};
+  if (req.files != null) {
     await cloudinary.v2.uploader.upload(req.files.file.tempFilePath,
     { public_id: req.files.file.name }, 
     function(error, result) {
@@ -25,18 +27,25 @@ router.post("/profile/edit", userAuth, async (req, res) => {
       }
       imageURL = result; 
     });
-    let response = await pool.query("UPDATE users SET image = $1 WHERE id = $2 RETURNING image;", [imageURL, req.user.id]);
-    let usersData = response.rows;
-    console.log(usersData)
-    res.json(usersData);
+    // only if image is uploaded, save the url to the db
+    let image = await pool.query("UPDATE users SET image = $1 WHERE id = $2 RETURNING image;", [imageURL, req.user.id]);
+    let imageData = image.rows;
+    console.log(imageData)
+    userInformation.imageData = imageData;
   }
-  
-    let response = await pool.query("UPDATE users SET username = $1, name = $2, bio = $3 WHERE id = $4 RETURNING username, name, bio;", [req.body.username, req.body.name, req.body.bio, req.user.id]);
-    let usersData = response.rows;
-    console.log(usersData)
-    res.json(usersData);
+    // let country = await pool.query("UPDATE countries SET countryName = $1 WHERE users.country_id = $2 RETURNING countryName;", [req.body.country, req.user.id]);
+    // let countryData = country.rows;
+    // console.log(countryData)
+    // res.json(countryData);
+
+    //save user info(usename, name, bio) to the db
+    let userInfo = await pool.query("UPDATE users SET username = $1, name = $2, bio = $3 WHERE id = $4 RETURNING username, name, bio;", [req.body.username, req.body.name, req.body.bio, req.user.id]);
+    let userData = userInfo.rows;
+    console.log(userData)
+    userInformation.userData = userData;
+    res.json(userInformation);
   } catch (err) {
-    console.log(err.message);
+    console.log(err);
   }
 });
 
