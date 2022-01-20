5cab4e1b161d96dee0950d0b4cf291531c7b8393
jtardioli <joshua.tardioli@gmail.com>
2022-01-19 19:21:25 -0500

    broken master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import AllConversationsPage from "./components/AllConversationsPage";
 import ProfilePage from "./components/ProfilePage";
 import AllProfilesPage from "./components/AllProfilesPage";
 import EditProfilePage from "./components/EditProfilePage";
-import PartnerProfile from "./components/PartnerProfile";
 
 const App = () => {
   return (
@@ -13,8 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<AllProfilesPage />}></Route>
         <Route path="/conversations" element={<AllConversationsPage />}></Route>
-        <Route path="/profile/" element={<ProfilePage />}></Route>
-        <Route path="/partner-profile/:id" element={<PartnerProfile />}></Route>
+        <Route path="/profile/:id" element={<ProfilePage />}></Route>
         <Route path="/register" element={<RegisterPage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/all-profiles" element={<AllProfilesPage />}></Route>
