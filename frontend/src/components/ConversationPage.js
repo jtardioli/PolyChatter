d7bfb64b8a12beffa267fe64d687f2c5f30a588d
jtardioli <joshua.tardioli@gmail.com>
2022-01-20 12:59:38 -0500

    set up socket.io connection

--- frontend/src/components/ConversationPage.js
+++ frontend/src/components/ConversationPage.js
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:5000");
 export const ConversationPage = () => {
   const { id } = useParams();
   return <div>This is the conversation with id: {id}</div>;
