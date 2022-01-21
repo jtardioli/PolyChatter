9cdaf61c91cfde07351de33576cc3663eac9b9e5
Justin Tu <coppolavina@gmail.com>
2022-01-21 20:38:35 +0000

    add chatting feature

--- frontend/src/components/ConversationPage.js
+++ frontend/src/components/ConversationPage.js
@@ -1,7 +1,79 @@
+import React, {useState, useEffect} from "react";
+import TextField from '@material-ui/core/TextField'
+import "../styles/layout/ConversationPage.scss";
 import { useParams } from "react-router-dom";
 import { io } from "socket.io-client";
 const socket = io("http://localhost:5000");
+
+
 export const ConversationPage = () => {
   const { id } = useParams();
-  return <div>This is the conversation with id: {id}</div>;
+
+  const [state, setState] = useState({message: "", name: ""})
+  const [chat, setChat] = useState([])
+
+  const onTextChange = e => {
+    setState({...state,[e.target.name]: e.target.value})
+  }
+
+  useEffect(() => {
+    socket.on('message',({name, message}) => {
+      setChat([...chat, {name,message}])
+    })
+  })
+
+  const onMessageSubmit = e => {
+    e.preventDefault()
+    const {name, message} = state
+    socket.emit('message', {name, message})
+    setState({message: "",name})
+  }
+
+
+  const renderChat = () => {
+    return chat.map (({name, message}, index) => (
+      <div key={index}>
+        <h3>
+            {name}: <span>{message}</span>
+        </h3>    
+      </div>
+    ))
+  }
+
+  return (
+  <>
+    {/* <div>This is the conversation with id: {id}</div>
+    <button onClick = {() => { socket.emit('chat message')}}>click me</button> */}
+
+    <div className ="card">
+      <form onSubmit={onMessageSubmit}>
+        <h1>Messenger</h1>
+        <div className ="name-field">
+          <TextField
+            name="name"
+            onChange={e => onTextChange(e)}
+            value={state.name}
+            label="Name"
+          />
+        </div>
+        <div className="name-field">
+          <TextField
+          name="message"
+          onChange={e => onTextChange(e)}
+          value={state.message}
+          id="outlined-multiline-static"
+          variant="outlined"
+          label="Message"
+          /> <button>translate</button>
+        </div>
+        <button> Send Message</button>
+      </form>
+      <div className="render-chat">
+        <h1>Chat Log</h1>
+        {renderChat()}<button>translate</button>  
+      </div>
+      
+    </div>  
+  </>
+  )
 };
