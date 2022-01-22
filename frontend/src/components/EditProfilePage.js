1a901fb7a08d2e7a7330e7e65f473deb8a4bf568
Gulbike Biyekenova <gbknva@gmail.com>
2022-01-22 03:08:01 +0000

    initial css of edit profile page

--- frontend/src/components/EditProfilePage.js
+++ frontend/src/components/EditProfilePage.js
@@ -1,11 +1,11 @@
 import Navbar from "./layout/Navbar";
 import Header from "./layout/Header";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import axios from 'axios';
-import { useNavigate } from "react-router-dom";
 
-import "../styles/layout/EditProfilePage.scss";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+
+import "../styles/EditProfilePage.scss";
 
 
 const EditProfilePage = () => {
@@ -90,37 +90,38 @@ const EditProfilePage = () => {
   return (
     <div>
       <Header />
-      <p>This is the EditProfilePage</p>
       <Navbar />
       <div> 
         
       </div>
     
       <form className="vertical">
-      <img src={user.image}  width="200" height="200" />
+      <img className="half" src={user.image}  width="120" height="120" />
       
-      <label>
+      <label className="label">
           Image
           <input 
             onChange={handleInputChange}
             // onChange={handleInput}
             type="file"
             name="image"
+            className="input"
 
           />
         </label>
       
-      <label>
+      <label className="label">
           Name
           <input
             onChange={handleInput}
             type="text"
             name="name"
             value={user.name}
+            className="input"
           />
         </label>
 
-        <label>
+        <label className="label">
           Username
           <input
             //onChange={(e) => setUsername(e.target.value)}
@@ -128,31 +129,11 @@ const EditProfilePage = () => {
             type="text"
             name="username"
             value={user.username}
+            className="input"
           />
         </label>
 
-        <label>
-          Self-introduction
-          <input
-            // onChange={(e) => setBio(e.target.value)}
-            onChange={handleInput}
-            type="text"
-            name="bio"
-            value={user.bio}
-          />
-        </label>
-
-        <label>
-          Country
-          <input
-            // onChange={(e) => setCountry(e.target.value)}
-            onChange={handleInput}
-            type="text"
-            name="countryData"
-            value={user.countryData}
-          />
-        </label>
-        <label>
+        <label className="label">
           Native Language
           <input
             // onChange={(e) => setNativeLanguage(e.target.value)}
@@ -160,9 +141,11 @@ const EditProfilePage = () => {
             type="text"
             name="nativeLanguage"
             value={user.nativeLanguage}
+            className="input"
           />
         </label>
-        <label>
+
+        <label className="label">
           Target Language
           <input
             // onChange={(e) => setTargetLanguage(e.target.value)}
@@ -170,10 +153,39 @@ const EditProfilePage = () => {
             type="text"
             name="targetLanguage"
             value={user.targetLanguage}
+            className="input"
           />
         </label>
+
+        <label className="label">
+          Country
+          <input
+            // onChange={(e) => setCountry(e.target.value)}
+            onChange={handleInput}
+            type="text"
+            name="countryData"
+            value={user.countryData}
+            className="input"
+          />
+        </label>
+
+        <label className="label">
+          Self-introduction
+          <input
+            // onChange={(e) => setBio(e.target.value)}
+            onChange={handleInput}
+            type="text"
+            name="bio"
+            value={user.bio}
+            className="input"
+            placeholder="Add Bio"
+            id="bio"
+          />
+        </label>
+
+        
+      <button className="btn" onClick={submit}>Save</button>
       </form>
-      <button onClick={submit}>Save</button>
     </div>
   );
 };
