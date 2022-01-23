c4f40a9d49dbf1ef9d82111db4b97ac3cd9adde1
jtardioli <joshua.tardioli@gmail.com>
2022-01-23 18:17:50 -0500

    merge with amster

--- frontend/src/components/PartnerMessage.js
+++ frontend/src/components/PartnerMessage.js
@@ -4,29 +4,44 @@ import axios from "axios";
 const API_KEY = "AIzaSyCP062q296wGvTMRHD0B5TNMuR44VFRQNc";
 const PartnerMessage = (props) => {
   const [translatedText, setTranslatedText] = useState("");
+  const sanitizeTranslation = (text) => {
+    return text.replace("&#39;", "'");
+  };
 
   const translateText = () => {
-    axios
-      .post(
-        "https://translation.googleapis.com/language/translate/v2",
-        {},
-        {
-          params: {
-            q: props.message,
-            target: "en",
-            key: "AIzaSyCP062q296wGvTMRHD0B5TNMuR44VFRQNc",
-          },
-        }
-      )
-      .then((response) => {
-        setTranslatedText(response.data.data.translations[0].translatedText);
-      })
-      .catch((err) => {
-        console.log("rest api error", err);
-      });
+    if (!translatedText) {
+      axios
+        .post(
+          "https://translation.googleapis.com/language/translate/v2",
+          {},
+          {
+            params: {
+              q: props.message,
+              target: "en",
+              key: "AIzaSyCP062q296wGvTMRHD0B5TNMuR44VFRQNc",
+            },
+          }
+        )
+        .then((response) => {
+          const newlang = sanitizeTranslation(
+            response.data.data.translations[0].translatedText
+          );
+          setTranslatedText(newlang);
+        })
+        .catch((err) => {
+          console.log("rest api error", err);
+        });
+    } else {
+      setTranslatedText("");
+    }
   };
   return (
     <div className="partner-message" onClick={translateText}>
+      {/* <div>
+        <span id="translate" class="material-icons material-icons-outlined">
+          translate
+        </span>
+      </div> */}
       <p>{props.message}</p>
       {translatedText && <p className="translate">{translatedText}</p>}
     </div>
