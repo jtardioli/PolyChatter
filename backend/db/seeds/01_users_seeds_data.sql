0e0102365da6791238e45a8347aef0d6389dc77b
Justin Tu <coppolavina@gmail.com>
2022-01-26 00:44:11 +0000

    add profile pictures and bios

--- backend/db/seeds/01_users_seeds_data.sql
+++ backend/db/seeds/01_users_seeds_data.sql
@@ -10,11 +10,11 @@ INSERT INTO users (
 VALUES (
   'bpaul',
   'Brenda Paul',
-  'https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png',
+  'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
   'Brenda@gmail.com',
   'Brenda123',
   1,
-  'Passionate polyglot, speaking 5 languages.'
+  'I am passionate about learning new things and making an impact on the organization. My passion also lie in people development and coaching. As a listener with a business mindset, customer-oriented approach and a point of contact in every single HR issues, I love to contribute my voice to employee engagement, business agility and performance. Outside of work, I spend time on enjoying building authentic relationships, reading books, music, singing,  travelling and enhancing my learning & challenges to grow.'
 );
 
 INSERT INTO users (
@@ -28,11 +28,12 @@ INSERT INTO users (
 VALUES (
   'gvance',
   'Giselle Vance',
-  'https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png',
+  'https://images.pexels.com/photos/718978/pexels-photo-718978.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
   'Giselle@gmail.com',
   'Giselle123',
   2,
-  'Passionate polyglot, speaking 5 languages.'
+  'Web developer and web and graphic designer with 4 years experience in web and graphic design. Driven to be involved in global interaction. Working on borderless websites and applications will allow me to grow exponentially.
+I am ready for the next exciting challenge, and look forward to problem solving with a team.'
 );
 
 INSERT INTO users (
@@ -46,11 +47,13 @@ INSERT INTO users (
 VALUES (
   'nconway',
   'Nigel Conway',
-  'https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png',
+  'https://images.pexels.com/photos/2285991/pexels-photo-2285991.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
   'Nigel@gmail.com',
   'Nigel123',
   3,
-  'Passionate polyglot, speaking 5 languages.'
+  'Executive of a marketing research firm specializing in product development research, price elasticity and demand forecasting, choice-based modeling, brand measurement and tracking, market segmentation, customer loyalty, and other custom research development for a variety of industries, with particular expertise in medical devices, clinical diagnostics, life sciences, cloud computing and HPC software, pharmaceuticals, manufacturing, and other information technology.
+
+Specialties: Marketing research planning, design and execution, marketing development, sales methodology development, sales force development and sales training.'
 );
 
 INSERT INTO users (
@@ -64,11 +67,13 @@ INSERT INTO users (
 VALUES (
   'ksimon',
   'Keely Simon',
-  'https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png',
+  'https://images.unsplash.com/photo-1639747280804-dd2d6b3d88ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
   'Keely@gmail.com',
   'Keely123',
   4,
-  'Passionate polyglot, speaking 5 languages.'
+  'I see myself as ambitious, self-made, detail-oriented and diligent person who believes in teamwork and has a good learning attitude with an aspiration to forge ahead of competition.
+
+Ambitious logistics professional with strong track-record of delivering top performance. Possess large spectrum of experience in Logistics. Can undertake difficult mandates and meet tight deadlines. Personal qualities include: rigorous, positive, good problem-solver, autonomous, persistent, adaptable, and drive. Aspire to obtain the position in a respectable firm where I can fully utilized my talents, knowledge, proficiency and excellent communication and interpersonal skills for the benefits of the firm.'
 );
 
 INSERT INTO users (
@@ -82,11 +87,11 @@ INSERT INTO users (
 VALUES (
   'kleon',
   'Leon Peters',
-  'https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png',
+  'https://images.pexels.com/photos/7870295/pexels-photo-7870295.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
   'Leon@gmail.com',
   'Leon123',
   5,
-  'Passionate polyglot, speaking 5 languages.'
+  'Leon Peters is the founder, former Chairman and long time CEO of Sanara Bread Company. Today,  through his investment vehicle Act III Holdings and in his role as a Board leader, is directing a range of long term strategic investments in consumer facing growth companies like Cook Business Network, Sar Technologies, UI’s Restaurants, Qava, Latte, Miu Miu Café, DIU and HiBre. He is also involved in a range of civic activities through his personal philanthropy, his commitment to education and his Board involvement in organizations like No Pabels and UAA which are working to reduce the political polarization in America today.'
 );
 INSERT INTO users (
   username,
@@ -98,12 +103,13 @@ INSERT INTO users (
   bio)
 VALUES (
   'caiden',
-  'Jaiden Cabrera',
-  'https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png',
+  'Jen Cabrera',
+  'https://i.pinimg.com/564x/1a/a4/58/1aa45803e797653e20412427f5c0857e.jpg',
   'Jaiden@gmail.com',
   'Jaiden123',
   6,
-  'Passionate polyglot, speaking 5 languages.'
+  'A energetic leader and a proven performer who has consistently developed high performing Sales Teams over the past 20+ years and delivered consistent results-Over 20 years as an executive in the software, IT Services and Print Strategy industries. '
 );
+
 
 
